39eec47b3b70f435e7cb92925074c3579bb920d9
sastra12 <anonymousonly594@gmail.com>
2024-03-06 07:55:01 +0700

    Solution for Defanging an IP Address Problem

---
+++ easy/defangIPaddr.php
@@ -0,0 +1,25 @@
+<?php
+// Defanging an IP Address
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+// Example 1:
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+// Example 2:
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+function defangIPaddr($address)
+{
+  $str = "";
+  for ($i = 0; $i < strlen($address); $i++) {
+    if ($address[$i] == ".") {
+      $str .= "[.]";
+    } else {
+      $str .= $address[$i];
+    }
+  }
+  return $str;
+}
+
+var_dump(defangIPaddr("1.1.1.1"));
