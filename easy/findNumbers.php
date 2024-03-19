b2dd1d818ca44a630ab586f15654d0d547b2cf57
sastra12 <anonymousonly594@gmail.com>
2024-03-19 14:21:06 +0700

    Solution for Find Numbers with Even Number of Digits Problem

---
+++ easy/findNumbers.php
@@ -0,0 +1,31 @@
+<?php
+// Find Numbers with Even Number of Digits
+// Given an array nums of integers, return how many of them contain an even number of digits.
+// Example 1:
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+// Explanation: 
+// 12 contains 2 digits (even number of digits). 
+// 345 contains 3 digits (odd number of digits). 
+// 2 contains 1 digit (odd number of digits). 
+// 6 contains 1 digit (odd number of digits). 
+// 7896 contains 4 digits (even number of digits). 
+// Therefore only 12 and 7896 contain an even number of digits.
+
+// Example 2:
+// Input: nums = [555,901,482,1771]
+// Output: 1 
+// Explanation: 
+// Only 1771 contains an even number of digits.
+function findNumbers($nums)
+{
+  $sum = 0;
+  foreach ($nums as $value) {
+    $lengthValue = strlen((string) $value);
+    if ($lengthValue % 2 == 0) {
+      $sum += 1;
+    }
+  }
+  return $sum;
+}
+var_dump(findNumbers([12, 345, 2, 6, 7896]));
